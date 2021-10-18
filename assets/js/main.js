984da6596074cce806b69ac108e29652a8feae73
Daniel <bolgardani@gmail.com>
2021-10-18 21:35:37 +0200

    particles added

--- assets/js/main.js
+++ assets/js/main.js
@@ -260,4 +260,9 @@
     }
   });
 
+  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+  particlesJS.load('particles-js', 'assets/vendor/particles/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
 })()
