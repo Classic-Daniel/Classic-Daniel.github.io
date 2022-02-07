59780995255cbd420c030bcb1f4dc1e0d41ad5a6
Classic-Daniel <bolgardani@gmail.com>
2022-02-07 14:30:04 +0100

    Finalized todos decryption

--- assets/js/decryption.js
+++ assets/js/decryption.js
@@ -1,24 +1,31 @@
-// console.log("decryption ran")
+const decryptWithAES = (ciphertext) => {
+    const passphrase = key;
+    const bytes = CryptoJS.AES.decrypt(ciphertext, passphrase);
+    const originalText = bytes.toString(CryptoJS.enc.Utf8);
+    return originalText;
+  };
 
-// var iv  = CryptoJS.enc.Utf8.parse('1583288699248111');
-// var key = CryptoJS.enc.Utf8.parse('b75524255a7f54d2726a951bb39204df');
-// var encryptedText = document.getElementById("todos").innerHTML;
+function decryptFail(){
+    alert("Wrong Password");
+}
 
-// //Decode from text    
-// var cipherParams = CryptoJS.lib.CipherParams.create({
-//      ciphertext: CryptoJS.enc.Base64.parse(encryptedText )
-// });
-// var decryptedFromText = CryptoJS.AES.decrypt(cipherParams, key, { iv: iv});
-// console.log(decryptedFromText.toString(CryptoJS.enc.Utf8));
-
-// // document.getElementById("todos").innerHTML = decryptedText;
-
-var encryptedText = document.getElementById("todos").innerHTML;
-console.log("Decryption");
-// console.log(encryptedText);
-
-// var encryptedAES = CryptoJS.AES.encrypt("MessageInOUT", "b75524255a7f54d2726a951bb39204df");
-var decryptedBytes = CryptoJS.AES.decrypt(encryptedText, "mindlikewater");
-var plaintext = decryptedBytes.toString(CryptoJS.enc.ascii);
-// console.log(encryptedText);
-console.log(plaintext);
+  function runDecryption() {
+      try {          
+            var encryptedText = document.getElementById("todos").innerHTML.replace(/\s/g, "");
+            key = document.getElementById("decryptKey").value;
+            
+            console.log("Decryption started...");
+            decryptedText = decryptWithAES(encryptedText);
+            
+            if(decryptedText.startsWith("<div")){
+                document.getElementById("todos").innerHTML = decryptedText;
+                document.getElementById("todos").classList = {};
+                document.getElementById("decryptForm").style.display = "none";
+            }else
+            {
+                decryptFail();
+            }
+      } catch (error) {
+        decryptFail();
+      }
+  }
