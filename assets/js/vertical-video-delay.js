d26a79863562a5b093535a431b2eee8e916434a8
Classic-Daniel <bolgardani@gmail.com>
2022-02-18 14:11:56 +0100

    vertical video delay top line issue solved

--- assets/js/vertical-video-delay.js
+++ assets/js/vertical-video-delay.js
@@ -13,66 +13,34 @@ console.log("imageBuffer length: " + imageBuffer.length);
 var currentIndex = -1;
 
 function addNewImage(image){
-    currentIndex = (currentIndex + 1) % BUFFER_SIZE;
     imageBuffer[currentIndex] = image.clone();
+    currentIndex = (currentIndex + 1) % BUFFER_SIZE;
     if(imageBuffer[BUFFER_SIZE - 1] != undefined){
       bufferReady = true;
     }
 }
 
-function getProcessedImage(){
-    // let generatedImage = new cv.Mat(video.height, video.width, cv.CV_8UC4);
-    let generatedImage = imageBuffer[currentIndex].clone();
-    // console.log(generatedImage);
-    // bottomImage = imageBuffer[Math.max((currentIndex - 10), 0)];
-    // bottomImage = cv.colRange(generatedImage, 100, 200);
-
-    heightStep = Math.round(video.height / BUFFER_SIZE);
+function getProcessedImage(currentImage){
+    let generatedImage = currentImage.clone();
 
     if(bufferReady){
-      for(let part = 1; part < BUFFER_SIZE; part++)
+      heightStep = Math.round(video.height / BUFFER_SIZE);
+      for(let part = 0; part < BUFFER_SIZE - 1; part++)
       {
         partFirstRow = heightStep * part;
         bufferIndex = (currentIndex + part) % BUFFER_SIZE;
         for (let j = partFirstRow; j < partFirstRow + heightStep; j++)
         {
-          for (let i = 1; i < video.width; i++)
-          {              
-            // generatedImage.ucharPtr(j, i)[0] = 255;
-            // generatedImage.ucharPtr(j, i)[1] = 255;
-            // generatedImage.ucharPtr(j, i)[2] = 255;
+          for (let i = 0; i < video.width; i++)
+          {
             generatedImage.ucharPtr(j, i)[0] = imageBuffer[bufferIndex].ucharPtr(j, i)[0];
             generatedImage.ucharPtr(j, i)[1] = imageBuffer[bufferIndex].ucharPtr(j, i)[1];
             generatedImage.ucharPtr(j, i)[2] = imageBuffer[bufferIndex].ucharPtr(j, i)[2];
           }
         }
       }
-
-      // for (let j = 1; j < video.height/3; j++)
-      // {
-      //   for (let i = 1; i < video.width; i++)
-      //   {              
-      //     generatedImage.ucharPtr(j, i)[0] = 255;
-      //     generatedImage.ucharPtr(j, i)[1] = 255;
-      //     generatedImage.ucharPtr(j, i)[2] = 255;
-      //       // generatedImage.ucharPtr(j, i)[0] = imageBuffer[bufferIndex].ucharPtr(j, i)[0];
-      //       // generatedImage.ucharPtr(j, i)[1] = imageBuffer[bufferIndex].ucharPtr(j, i)[1];
-      //       // generatedImage.ucharPtr(j, i)[2] = imageBuffer[bufferIndex].ucharPtr(j, i)[2];
-      //   }
-      // }
     }
-    
-    // for (let i = 1; i < BUFFER_SIZE; i++) {
-    //     // create temporary image that will hold the mask
-    //     mask = cv.Mat(video.height, video.width, CV_8UC1, Scalar(0));
-    //     cv2.rectangle(mask, (0, 0), (100, 100), (255), -1);
-    //     imageBuffer[i].copyTo(generatedImage, mask);
-    // }
-    // return imageBuffer[0];
-    // heightStep = Math.round(video.height / BUFFER_SIZE);
-    // let mask = new cv.Mat(video.height, video.width, cv.CV_8UC1, [0, 0, 0, 0]);
-    // cv.rectangle(mask, [0, 0], [100, 100], [122], -1);
-    // return mask;
+    addNewImage(currentImage);
     return generatedImage;
 }
 
@@ -99,8 +67,7 @@ navigator.mediaDevices
         // start processing.
         cap.read(src);
         // cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
-        addNewImage(src);
-        dst = getProcessedImage()
+        dst = getProcessedImage(src);
 
         cv.imshow("canvasOutput", dst);
         // schedule the next one.
